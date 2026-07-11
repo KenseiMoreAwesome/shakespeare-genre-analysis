54c19b8984c8a6576234b9a3b6d2d1f4c4635c7e
Kensei <kensei.c134@hotmail.com>
2026-07-11 17:39:15 +0000

    Fixed script

--- analysis.py
+++ analysis.py
@@ -1,12 +1,12 @@
 import nltk
-nltk.download('punkt')
+nltk.download('punkt', quiet=True)
 
 # prep the data
 plays = {
     'Comedy': ['asyoulikeit', 'amidsummernightsdream', 'muchadoaboutnothing', 'twelthnight'],
     'Tragedy': ['macbeth', 'othello', 'kinglear', 'hamlet'],
     'History': ['henryivpart1', 'henryv', 'henryvipart2', 'richardii'],
-    'Testing': []
+    'Testing': ['macbeth', 'othello', 'kinglear', 'hamlet', 'asyoulikeit', 'amidsummernightsdream', 'muchadoaboutnothing', 'twelthnight', 'henryivpart1', 'henryv', 'henryvipart2', 'richardii']
 }
 
 def read_files_into_string(filenames):
@@ -14,47 +14,42 @@ def read_files_into_string(filenames):
     for filename in filenames:
         with open(f'control_corpus_clean/{filename}.txt') as f:
             strings.append(f.read())
-        return '\n'.join(strings)
+    return '\n'.join(strings)
     
 plays_by_genre = {}
 for genre, files in plays.items():
     plays_by_genre[genre] = read_files_into_string(files)
 
 for genre in plays:
-    print(plays_by_genre[genre][:100])
+    print(f"{genre}: {plays_by_genre[genre][:100]!r}")
 
-#Tokenise
-genres = ('Comedy', 'History', 'Tragedy')
+# Tokenize all genres, including Testing
 plays_by_genre_tokens = {}
+for genre, text in plays_by_genre.items():
+    plays_by_genre_tokens[genre] = [token.lower() for token in nltk.word_tokenize(text)]
+
+# Kilgarriff’s Chi-Squared Method
+genres = ('Comedy', 'History', 'Tragedy')
+
 for genre in genres:
-    tokens = nltk.word_tokenize(plays_by_genre[genre])
+    joint_corpus = plays_by_genre_tokens[genre] + plays_by_genre_tokens['Testing']
+    joint_freq_dist = nltk.FreqDist(joint_corpus)
+    most_common = list(joint_freq_dist.most_common(500))
 
-# Kilgariff’s Chi-Squared Method
-#Lowercasing
-for genre in genre:
-    plays_by_genre_tokens[genre] = ([token.lower() for token in plays_by_genre_tokens[genre]])
-plays_by_genre_tokens['Testing'] = ([token.lower() for token in plays_by_genre_tokens['Testing']])
+    genre_share = len(plays_by_genre_tokens[genre]) / len(joint_corpus)
 
-#Calculating Chi-Squared value
-for genre in genres:
-    ## Building joint corpus
-    joint_corpus = (plays_by_genre_tokens[genre] + plays_by_genre_tokens['Testing'])
-    join_freq_dist = nltk.FreqDist(joint_corpus)
-    most_common = list(join_freq_dist.most_common(500))
-
-    genre_share = (len(plays_by_genre_tokens[genre]) / len(joint_corpus))
-
-    ## Determines the frquency of certain words in genre compared to what would be expected
-    chisquared = 0
-    for word,joint_count in most_common:
+    chisquared = 0.0
+    for word, joint_count in most_common:
         genre_count = plays_by_genre_tokens[genre].count(word)
         testing_count = plays_by_genre_tokens['Testing'].count(word)
 
         expected_genre_count = joint_count * genre_share
-        expected_testing_count = joint_count * (1-genre_share)
+        expected_testing_count = joint_count * (1 - genre_share)
 
-    chisquared += ((genre_count - expected_genre_count) * (genre_count - expected_genre_count) / expected_genre_count)
-    chisquared += ((testing_count - expected_testing_count) * (testing_count - expected_testing_count) / expected_testing_count )
+        if expected_genre_count > 0:
+            chisquared += ((genre_count - expected_genre_count) ** 2) / expected_genre_count
+        if expected_testing_count > 0:
+            chisquared += ((testing_count - expected_testing_count) ** 2) / expected_testing_count
 
-    print(f'The Chisquared stat for genre {genre} is {chisquared}')
+    print(f'The Chi-squared stat for genre {genre} is {chisquared:.3f}')
 
